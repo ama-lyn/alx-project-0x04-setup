c5692c8b2da4740e38718807eefc1e751cb91cdf
Gwendolyn <132227466+ama-lyn@users.noreply.github.com>
2025-06-11 16:25:31 +0300

    Update _app.tsx

--- alx-project-0x06/pages/_app.tsx
+++ alx-project-0x06/pages/_app.tsx
@@ -2,7 +2,7 @@ import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import { store } from "@/store/store"; 
+import store  from "@/store/store"; 
 import { CountProvider } from "@/context/CountContext"; 
 
 export default function App({ Component, pageProps }: AppProps) {
